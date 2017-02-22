1a8f39e4d51d1a6cdaa11862d6b15a3202f54281
Sebastián Estévez <estevezsebastian@gmail.com>
2017-02-21 19:56:55 -0500

    ToInt take2

---
+++ metagen-lib-basics/src/main/java/io/virtdata/ints/ToInt.java
@@ -0,0 +1,11 @@
+package io.virtdata.ints;
+
+import java.util.function.Function;
+
+public class ToInt implements Function<Object,Integer> {
+
+    @Override
+    public Integer apply(Object o) {
+        return Integer.valueOf(o);
+    }
+}
