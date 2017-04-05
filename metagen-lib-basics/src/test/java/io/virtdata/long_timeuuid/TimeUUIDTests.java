2ee5657f777b33db5eb85f3240207e4929e45dab
Jonathan Shook <jshook@gmail.com>
2017-04-05 09:05:07 -0500

    reltime work in progress

--- metagen-lib-basics/src/test/java/io/virtdata/long_timeuuid/TimeUUIDTests.java
+++ metagen-lib-basics/src/test/java/io/virtdata/long_timeuuid/TimeUUIDTests.java
@@ -11,6 +11,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TimeUUIDTests {
 
+    private final DateTime calStart = new DateTime(
+            1582,10,15,
+            0,0,0,
+            DateTimeZone.UTC
+    );
+    private final DateTime refTime = new DateTime(
+            2015, 5, 11,
+            23, 23, 23,
+            DateTimeZone.UTC
+    );
+
+    private final String may5late= "2015-05-11 23:23:23";
+    private final String gregsCalendarStart = "1582-10-15 00:00:00";
+    private final String aLittleAfterGregsTime = "1583-10-15 00:00:00";
+
     @Test
     public void testFinestTimeUUID() {
         ToFinestTimeUUID tjtu = new ToFinestTimeUUID(234,567);
@@ -40,6 +55,23 @@ public class TimeUUIDTests {
         long expected = interval.toDuration().getMillis() * 10000;
         assertThat(uuid.timestamp()).isEqualTo(expected);
     }
+
+    @Test
+    public void testEpochBaseTimeUUID() {
+        ToEpochTimeUUID atStartOftime = new ToEpochTimeUUID(aLittleAfterGregsTime);
+        UUID uuid = atStartOftime.apply(0);
+        assertThat(uuid.timestamp()).isEqualTo(0);
+    }
+
+    @Test
+    public void testFinestBaseTimeUUID() {
+        ToFinestTimeUUID tetu = new ToFinestTimeUUID(gregsCalendarStart);
+        UUID uuid = tetu.apply(0);
+        assertThat(uuid.timestamp()).isEqualTo(0L);
+        uuid = tetu.apply(2);
+        assertThat(uuid.timestamp()).isEqualTo(2L);
+    }
+
 
     @Test
     public void sanityCheckHostGen() {
