f188585658d98c19bd96cc1217c6bb4842e841c7
Christopher Lambert <Christopher.Lambert@Datastax.com>
2017-11-09 13:38:06 +0100

    add AlphaNumericString

---
+++ metagen-lib-basics/src/test/java/io/virtdata/long_string/AlphaNumericStringTest.java
@@ -0,0 +1,43 @@
+package io.virtdata.long_string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import io.virtdata.basicsmappers.from_long.to_string.AlphaNumericString;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AlphaNumericStringTest
+{
+    @Test
+    public void testAlphaNumericStringZeroLength()
+    {
+        AlphaNumericString alphaNumeric = new AlphaNumericString(0);
+        for (long cycle = 0; cycle < 5; cycle++)
+        {
+            String value = alphaNumeric.apply(cycle);
+            assertThat(value).isEqualTo("");
+        }
+    }
+
+    @Test
+    public void testAlphaNumericStringBasic()
+    {
+        AlphaNumericString alphaNumeric = new AlphaNumericString(20);
+        Set<String> seen = new HashSet<>();
+        for (long cycle = 0; cycle < 5000; cycle++)
+        {
+            String value = alphaNumeric.apply(cycle);
+            assertThat(value.length()).isEqualTo(20);
+            assertThat(seen).doesNotContain(value);
+            seen.add(value);
+        }
+        for (long cycle = 0; cycle < 5000; cycle++)
+        {
+            String value = alphaNumeric.apply(cycle);
+            assertThat(value.length()).isEqualTo(20);
+            assertThat(seen).contains(value);
+        }
+    }
+}
