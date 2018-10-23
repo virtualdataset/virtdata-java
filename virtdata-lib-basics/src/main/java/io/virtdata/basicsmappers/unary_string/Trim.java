c927b4b112109ad2094a43292f6640b1c784b486
Jeremy Hanna <jeromatron@gmail.com>
2018-10-23 17:23:02 -0500

    Added a method to trim a given string of surrounding whitespace.

---
+++ virtdata-lib-basics/src/main/java/io/virtdata/basicsmappers/unary_string/Trim.java
@@ -0,0 +1,22 @@
+package io.virtdata.basicsmappers.unary_string;
+
+import io.virtdata.annotations.Example;
+import io.virtdata.annotations.ThreadSafeMapper;
+
+import java.util.function.Function;
+
+/**
+ * Trim the input value and return the result.
+ */
+@ThreadSafeMapper
+public class Trim implements Function<String, String>{
+
+    @Example({"Trim('HashedLineToString('foo')')", "Trims the input value of all whitespace"})
+    public Trim(){
+    }
+
+    @Override
+    public String apply(String s) {
+        return s.trim();
+    }
+}
