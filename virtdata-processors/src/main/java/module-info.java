4a6d159a620d6216d0177045f8509e48f06b38b0
Jonathan Shook <jshook@gmail.com>
2019-04-10 15:54:09 -0500

    modularized virtdata-annotations

--- virtdata-processors/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module virtdata.processors {
-    exports io.virtdata.processors;
-    requires java.compiler;
-    requires com.squareup.javapoet;
-    requires virtdata.annotations;
-    provides javax.annotation.processing.Processor with
-            io.virtdata.processors.FunctionDocInfoProcessor,
-            io.virtdata.processors.ServiceProcessor;
-}
