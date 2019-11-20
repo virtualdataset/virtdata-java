12126b9601e3e8f136991d95023b7c42d4582920
Jonathan Shook <jshook@gmail.com>
2019-11-20 01:51:32 -0600

    added node docsys

---
+++ virtdata-docsys/src/main/node/docsys/components/DocsMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="menu">
+    <v-list dense>
+        <v-list-group  link v-for="category in categories" :key="category">
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title class="text-capitalize">{{category.category}}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+
+          <v-list-item
+            v-for="(doc, i) in category.docs"
+            :key="i"
+            link
+            :href="doc.filename"
+          >
+            <router-link :to="doc.filename">
+            <v-list-item-title >
+                  {{doc.attributes.title}}
+            </v-list-item-title>
+            </router-link>
+          </v-list-item>
+ 
+        </v-list-group>
+    </v-list>
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'DocsMenu',
+    props: [ 'categories' ]
+  }
+</script>
