ae21cf79cee53654ef62a29b01aaafbf0231e30d
Jonathan Shook <jshook@gmail.com>
2019-12-19 11:26:35 -0600

    doc updates

--- virtdata-docsys/src/main/java/io/virtdata/docsys/DocsysDefaultAppPath.java
+++ virtdata-docsys/src/main/java/io/virtdata/docsys/DocsysDefaultAppPath.java
@@ -4,7 +4,7 @@ import io.virtdata.docsys.api.Docs;
 import io.virtdata.docsys.api.DocsInfo;
 import io.virtdata.docsys.api.DocsysStaticManifest;
 
-//@Service(DocsysStaticManifest.class)
+//@Service(DocsysStaticManifest.class) loaded conditionally by explicit reference
 public class DocsysDefaultAppPath implements DocsysStaticManifest {
 
     @Override
